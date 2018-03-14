916449959958974517711fb6951aa2645da767f3
Jeremy Nagel <jnagel@cozero.com.au>
2018-03-14 11:50:02 +1100

    Fix grep issue

--- stylelint/stylelint.sh
+++ stylelint/stylelint.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
 echo "--- :css: stylelint SASS/SCSS/CSS linting"
 
 CMD="/linter/node_modules/.bin/stylelint"
+CONFIG=""
 
 if [ -e .stylelintrc ] \
     || [ -e stylelint.config.js ] \
     || grep --quiet stylelint package.json
 then
   echo "--- :css: found project config file"
+else
+  CONFIG="--config /linter/.stylelintrc"
 fi
 
 if [ "$#" -gt 0 ]; then
@@ -21,6 +24,6 @@ fi
 
 echo "Let's do this! Linting these files: $ARGS"
 
-$CMD $ARGS
+$CMD $CONFIG $ARGS
 
 echo "👌 LGTM!"
