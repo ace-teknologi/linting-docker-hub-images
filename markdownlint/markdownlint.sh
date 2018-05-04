2242ee9f2d7164bfe51e595141940e56625097e9
Joel Courtney <euphemize@gmail.com>
2018-05-04 14:42:42 +1000

    Feature: linter-markdownlint (#9)
    * markdownlint ftw
    * Let's use v1.1.0 not v0.3.0 ><
    * .travis.yml update to use new gcr.io/gcp-runtimes/container-structure-test method

---
+++ markdownlint/markdownlint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- :markdown: Markdown linting"
+
+CMD=/linter/node_modules/.bin/markdownlint
+CONFIG=""
+
+if [ -e .markdownlint.json ]; then
+  echo "--- :eslint: found project config"
+else
+  echo "--- :eslint: using default config"
+  CONFIG="-c /linter/.markdownlint.json"
+fi
+
+if [ "$#" -gt 0 ]; then
+  ARGS=$@
+else
+  ARGS="/app/*/*.md"
+fi
+
+echo "Let's do this! Linting these files: $ARGS"
+
+$CMD $CONFIG $ARGS
+
+echo "👌 LGTM!"
