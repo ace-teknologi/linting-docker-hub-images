649e8951ace7cb1ba08e4f9d4a65ca644bc91f36
Joel Courtney <euphemize@gmail.com>
2018-08-02 13:57:41 +1000

    Correct the YAML config

--- yamllint/yamllint.sh
+++ yamllint/yamllint.sh
@@ -4,16 +4,35 @@ set -euo pipefail
 
 echo "--- :yaml: YAML linting"
 
-echo "--- Called with args::"
-echo $@
-
-CMD=/usr/local/bin/yamllint
+CMD="/usr/local/bin/yamllint"
+CONFIG=""
+ARGS="."
 
 if [ -e .yamllintrc ] || [ -e yamllintrc ] || [ -e .yamllintrc.yaml ] || [ -e yamllintrc.yaml ]
 then
   echo "--- :yaml: found project config"
+  if [ -e .yamllintrc ] then
+    CONFIG="-c .yamllintrc"
+  fi
+  if [ -e yamllintrc ] then
+    CONFIG="-c yamllintrc"
+  fi
+  if [ -e .yamllintrc.yaml ] then
+    CONFIG="-c .yamllintrc.yaml"
+  fi
+  if [ -e yamllintrc.yaml ] then
+    CONFIG="-c yamllintrc.yaml"
+  fi
+else
+  CONFIG="-c /etc/yamllintrc.yaml"
 fi
 
-$CMD "$@"
+if [ "$#" -gt 0 ]; then
+  ARGS=$@
+fi
+
+echo "Let's do this! Linting these files: $ARGS"
+
+$CMD $CONFIG $ARGS
 
 echo "👌 LGTM!"
